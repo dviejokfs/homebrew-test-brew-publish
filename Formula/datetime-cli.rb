81da13728c7eda60459f25754b092f1d754bda79
David Viejo <dviejo@kungfusoftware.es>
2025-10-27 20:14:36 +0100

    Update repository URLs to homebrew-test-brew-publish

--- Formula/datetime-cli.rb
+++ Formula/datetime-cli.rb
@@ -1,13 +1,13 @@
 class DatetimeCli < Formula
   desc "A simple CLI tool to print the current date and time"
-  homepage "https://github.com/dviejokfs/test-brew-publish"
+  homepage "https://github.com/dviejokfs/homebrew-test-brew-publish"
   version "0.0.4"
 
   if Hardware::CPU.intel?
-    url "https://github.com/dviejokfs/test-brew-publish/releases/download/v0.0.4/datetime-cli-x86_64-apple-darwin.tar.gz"
+    url "https://github.com/dviejokfs/homebrew-test-brew-publish/releases/download/v0.0.4/datetime-cli-x86_64-apple-darwin.tar.gz"
     sha256 "3146fbb9fcb13fb5bdd8055a644c81f92a106b195e7a16fbf64ddc295073af60"
   else
-    url "https://github.com/dviejokfs/test-brew-publish/releases/download/v0.0.4/datetime-cli-aarch64-apple-darwin.tar.gz"
+    url "https://github.com/dviejokfs/homebrew-test-brew-publish/releases/download/v0.0.4/datetime-cli-aarch64-apple-darwin.tar.gz"
     sha256 "12166df1359215a36bda105535033f8cb01eeb949037ed0e2443de25affc76ab"
   end
 
